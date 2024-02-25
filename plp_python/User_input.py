aed0bede7c36e0ddcc0cc1a1ce3fd31d3172547f
faithsiele <158351735+faithsiele@users.noreply.github.com>
2024-02-25 21:01:48 +0300

    Add files via upload
    first commit

---
+++ plp_python/User_input.py
@@ -0,0 +1,7 @@
+# Ask user for their name, age, and location
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+# Print personalized message
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
